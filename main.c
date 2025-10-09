ae8a53f29bc8cd2089a31e827e75469fca104d4e
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-10-09 17:48:21 +0200

    ujJatek, utmutato, menu függvények hozzáadva. tesztelés hiba menüben

--- main.c
+++ main.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void ujJatek(){
+    printf("Új játék");
+}
+
+void utmutato(){
+    printf("Útmutató");
+}
+
+void menu(){
+    system("clear");
+    int menuPont;
+    int eredmeny;
+    do
+    {
+        printf("1. Új játék \n2. Gyorstalpaló \n3. Kilépés \n\nMenü pont: ");
+        eredmeny = scanf("%d", &menuPont);
+        if (eredmeny != 1)
+        {
+            printf("Hibás menüpont!");
+            continue;
+        }
+        
+        switch (menuPont)
+        {
+        case 1:
+            system("clear");
+            ujJatek();
+            break;
+        case 2:
+            system("clear");
+            utmutato();
+            break;
+        case 3:
+            system("clear");
+            printf("Kilépett az alkamazásból!");
+            exit(0);
+            break;
+        default:
+            system("clear");
+            printf("\nHibás menüpont!\n");
+            break;
+        }
+
+    } while (menuPont != 1 && menuPont != 2 && menuPont != 3);
+    
+}
 
 int main(void){
-
+    menu();
     return 0;
 }
