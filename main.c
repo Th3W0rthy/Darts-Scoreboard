eab0d35d70b6696e2ce651cece28676b1f3260bb
Th3W0rthy <nemethszabolcs.daniel02@gmail.com>
2025-10-20 17:38:47 +0200

    menü javítása, változó és függvény nevek javítása, uj_jatek_menu almenü létrehozása

--- main.c
+++ main.c
@@ -1,54 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void ujJatek(){
-    printf("Új játék");
-}
-
-void utmutato(){
-    printf("Útmutató");
-}
-
-void menu(){
-    system("clear");
-    int menuPont;
-    int eredmeny;
-    do
-    {
-        printf("1. Új játék \n2. Gyorstalpaló \n3. Kilépés \n\nMenü pont: ");
-        eredmeny = scanf("%d", &menuPont);
-        if (eredmeny != 1)
+void uj_jatek_menu() {
+    char menu_pont;
+    do {
+        printf("1. X01\n2. Krikett\n3. Vissza\n\nMenüpont:");
+        scanf(" %c", &menu_pont);
+        switch (menu_pont)
         {
-            printf("Hibás menüpont!");
-            continue;
-        }
-        
-        switch (menuPont)
-        {
-        case 1:
-            system("clear");
-            ujJatek();
+        case '1':
+            printf("X01");
             break;
-        case 2:
-            system("clear");
+        case '2':
+            printf("Krikett");
+            break;
+        case '3':
+            //Vissza
+            return;
+            break;
+        default:
+            printf("\nHibás menüpont!\n");
+            break;
+        }
+    } while (menu_pont != '3');
+}
+
+void utmutato() {
+    printf("Útmutató");
+    exit(0);
+}
+
+void mentett_jatekok() {
+    printf("Mentett játékok");
+    exit(0);
+}
+
+void fo_menu() {
+    char menu_pont;
+    do {
+        printf("1. Új játék \n2. Útmutató \n3. Mentett játékok \n4. Kilépés\n\nMenüpont: ");
+        scanf(" %c", &menu_pont);
+        switch (menu_pont)
+        {
+        case '1':
+            uj_jatek_menu();
+            break;
+        case '2':
             utmutato();
             break;
-        case 3:
-            system("clear");
+        case '3':
+            mentett_jatekok();
+            break;
+        case '4':
             printf("Kilépett az alkamazásból!");
             exit(0);
             break;
         default:
-            system("clear");
             printf("\nHibás menüpont!\n");
             break;
         }
 
-    } while (menuPont != 1 && menuPont != 2 && menuPont != 3);
+    } while (menu_pont != '4');
     
 }
 
-int main(void){
-    menu();
+int main(void) {
+    fo_menu();
     return 0;
 }
